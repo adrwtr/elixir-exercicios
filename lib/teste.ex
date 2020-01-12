979fab4c738e80076646cfea08720cc37fb1954d
Adriano Waltrick <akuma.majunia@gmail.com>
2020-01-12 18:24:17 -0300

    exericcio 2

--- lib/teste.ex
+++ lib/teste.ex
@@ -32,6 +32,16 @@ defmodule Teste do
             IO.puts("chave: #{k} =>  valor: #{x}")
         end
 
+
+        # // Escreva um programa que imprime na tela os numeros
+        # // de 1 a 30 exceto os numeros
+        # // múltiplos de 3.
+        for n <- 1..4 do
+            if ((rem n, 3) != 0) do
+                IO.puts n
+            end
+        end
+
     end
 
     def imprimir_valor(nr_valor) do
